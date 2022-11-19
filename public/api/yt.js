7d7436098363034c9f338509d929817e0f550b6f
Th3Skeleton <40904175+Th3Skeleton@users.noreply.github.com>
2022-11-19 12:40:16 -0500

    Add files via upload

---
+++ public/api/yt.js
@@ -0,0 +1,37 @@
+const { assert } = require("console");
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const contentDisposition = require("content-disposition");
+const information = {
+  title: "happy birthday kali, there isn't a clue here",
+};
+module.exports = (function () {
+  router.get("/", function (req, res, next) {
+    res.render("index");
+  });
+
+  router.get("/mp3/:url", async (req, res) => {
+    if (!req.params.url || req.params.url.length < 1) return false;
+    const {
+      player_response: {
+        videoDetails: { title, author },
+      },
+    } = await ytdl.getBasicInfo(req.params.url);
+
+    const file = ytdl(req.params.url, {
+      quality: "highestaudio",
+    });
+    res.setHeader(
+      "Content-Disposition",
+      contentDisposition(title + ".mp3"),
+      "Content-Type",
+      "audio/mp3",
+      "Cache-Control",
+      "must-revalidate"
+    );
+    file.pipe(res);
+  });
+  return router;
+})();
